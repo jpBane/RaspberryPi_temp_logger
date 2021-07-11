c0c30a28785024fbc35589f821dea3237113142b
jpBane <54914492+jpBane@users.noreply.github.com>
2021-07-11 21:12:30 +0200

    Initial commit.
    Installation script to create a temperature and humidity logger on a fresh PiZero. No solution for the autostart yet.

---
+++ temp_logger_instalation.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+
+# Update all packages to their newest version:
+sudo apt update && sudo apt upgrade
+
+# Set the correct timezone for Germany:
+sudo timedatectl set-timezone Europe/Berlin
+
+# Install required python packages
+sudo apt install python3-dev python3-pip
+sudo python3 -m pip install --upgrade pip setuptools wheel
+
+# Install the sensor package
+sudo pip3 install Adafruit_DHT
+
+# Get the script for the temperature/humidity logger
+cd /home/pi
+mkdir temp_humidity_logger
+wget https://raw.githubusercontent.com/jpBane/RaspberryPi_temp_logger/main/temp_humidity_logger.py
+
+# Make sure the script starts when the PiZero is booted.
