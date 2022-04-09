0aef16195733a0b9fd1a453961a292b36fa45613
jpBane <54914492+jpBane@users.noreply.github.com>
2022-04-09 22:55:04 +0200

    First version temp & humidity
    This is the first serious version. The cpu temperature that was used for testing was excluded and the humidity was added.

--- temp_humidity_logger_db.py
+++ temp_humidity_logger_db.py
@@ -1,6 +1,5 @@
 # Import required functions from packages
 from influxdb import InfluxDBClient
-from gpiozero import CPUTemperature
 import time
 import Adafruit_DHT 
 
@@ -8,15 +7,12 @@ import Adafruit_DHT
 client = InfluxDBClient(host='localhost', port=8086)
 client.switch_database('temp_logger_db')
 
-cpu = CPUTemperature()
-
 DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_PIN = 4
 
 humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
 
 # ...
-print(client.get_list_database())
 
 while True:
  if humidity is not None and temperature is not None:
@@ -27,9 +23,8 @@ while True:
             "location": "Keller"
         },
         "fields": {
-            "cpu_temp": cpu.temperature,
-            "air_temp": round(temperature, ndigits = 1)
-            
+            "temperature": round(temperature, ndigits = 1),
+            "humidity": round(humidity, ndigits = 1)            
         }
     }
   ]
@@ -38,4 +33,4 @@ while True:
 
  client.write_points(json_body, time_precision='s')
 
- time.sleep(5)
+ time.sleep(60)
