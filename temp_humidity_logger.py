0d77293f0eb28219e509defc3fcb8a5372c46534
jpBane <54914492+jpBane@users.noreply.github.com>
2022-06-16 15:04:46 +0200

    deleted the loop inside the python script
    It is better to do the looping with cron instead of having a python script run forever.

--- temp_humidity_logger.py
+++ temp_humidity_logger.py
@@ -1,10 +1,13 @@
+# Import the required functions from packages
 import os
 import time
 import Adafruit_DHT
 
+# Define the variables
 DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_PIN = 4
 
+# Define the CSV file to log the data.
 try:
  f = open("/home/pi/temp_humidity_logger/temp_humidity.csv", "a+")
  if os.stat("/home/pi/temp_humidity_logger/temp_humidity.csv").st_size == 0:
@@ -12,13 +15,11 @@ try:
   
 except:
  pass
- 
-while True:
- humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
- if humidity is not None and temperature is not None:
-  f.write("{0},{1:0.1f},{2:0.1f}\r\n".format(time.strftime("%Y-%m-%d %H:%M:%S"), temperature, humidity))
-  f.flush()
- else:
-  print("Failed to retrieve data from humidity sensor")
- 
- time.sleep(60)
+
+# Write the data to the CSV file
+humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+if humidity is not None and temperature is not None:
+ f.write("{0},{1:0.1f},{2:0.1f}\r\n".format(time.strftime("%Y-%m-%d %H:%M:%S"), temperature, humidity))
+ f.flush()
+else:
+ print("Failed to retrieve data from humidity sensor")
